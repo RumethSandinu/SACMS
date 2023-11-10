8e9258798c13a7644b8c6635f45fda443b56bf54
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2023-11-10 12:31:08 +0530

    Sample UI

--- src/main/java/com/example/sacms/UserRegMainController.java
+++ src/main/java/com/example/sacms/UserRegMainController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,13 +14,15 @@ public class UserRegMainController {
 
     @FXML
     private AnchorPane consoleAnchor;
+    @FXML
+    private AnchorPane studentAnchor;
 
     @FXML
     private void studentRegister() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(UserRegApplication.class.getResource("UserReg.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(UserRegApplication.class.getResource("student-reg.fxml"));
         Stage newStage = new Stage();
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        newStage.setTitle("Registration");
+        newStage.setTitle("Student Sign In");
         newStage.setScene(scene);
         newStage.show();
         Stage prevStage = (Stage) consoleAnchor.getScene().getWindow();
@@ -30,6 +33,15 @@ public class UserRegMainController {
     private void advisorRegister() throws IOException {
 
     }
-
-
+    @FXML
+    private void backButton() throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(UserRegApplication.class.getResource("UserReg.fxml"));
+        Stage mainStage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        mainStage.setTitle("Registration");
+        mainStage.setScene(scene);
+        mainStage.show();
+        Stage prevStage = (Stage) studentAnchor.getScene().getWindow();
+        prevStage.close();
+    }
 }
