4aee6ac9d13f36c8ddc8d08ce5581098db99c75a
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2023-11-10 12:18:30 +0530

    Sample UI

--- src/main/java/com/example/sacms/UserRegMainController.java
+++ src/main/java/com/example/sacms/UserRegMainController.java
@@ -1,16 +1,35 @@
 package com.example.sacms;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserRegMainController {
 
     @FXML
-    private void studentRegister(MouseEvent mouseEvent) {
+    private AnchorPane consoleAnchor;
+
+    @FXML
+    private void studentRegister() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(UserRegApplication.class.getResource("UserReg.fxml"));
+        Stage newStage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        newStage.setTitle("Registration");
+        newStage.setScene(scene);
+        newStage.show();
+        Stage prevStage = (Stage) consoleAnchor.getScene().getWindow();
+        prevStage.close();
     }
 
     @FXML
-    private void advisorRegister(MouseEvent mouseEvent) {
+    private void advisorRegister() throws IOException {
+
     }
+
+
 }
