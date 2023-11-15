c7e26959a8ff66506f58e7a99d0ddf0d75255f66
Rumeth Sandinu <sandinu413@gmail.com>
2023-11-15 21:06:34 +0530

    added event-scheduling

--- src/main/java/com/example/sacms/SACMSController_1.java
+++ src/main/java/com/example/sacms/SACMSController_1.java
@@ -2,6 +2,10 @@ package com.example.sacms;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class SACMSController_1 {
     @FXML
@@ -11,4 +15,5 @@ public class SACMSController_1 {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
