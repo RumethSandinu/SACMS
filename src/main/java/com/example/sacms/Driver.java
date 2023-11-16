70ad59613ea26392037d91bf73c07bc3b5c8eb9a
Rumeth Sandinu <sandinu413@gmail.com>
2023-11-16 10:58:47 +0530

    added event-view-ui

--- src/main/java/com/example/sacms/Driver.java
+++ src/main/java/com/example/sacms/Driver.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Driver extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("event-scheduling-ui.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("event-view-ui.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
